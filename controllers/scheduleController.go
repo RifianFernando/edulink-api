0a88d05097cb8a381afa3be2e54a5384e60b8566
Rifian Fernando <rifianfernando19@gmail.com>
2024-12-18 21:36:14 +0700

    feat(schedule): enhance schedule request validation and structure

--- controllers/scheduleController.go
+++ controllers/scheduleController.go
@@ -1,7 +1,33 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	req "github.com/edulink-api/request/schedule"
+	"github.com/edulink-api/res"
+	"github.com/gin-gonic/gin"
+)
 
 func GenerateAndCreateScheduleTeachingClassSubject(c *gin.Context) {
-	// var request InsertScheduleTeachingClassSubjectRequest
+	var request req.InsertScheduleRequest
+	var allErrors []map[string]string
+
+	// Bind the request JSON to the CreateStudentRequest struct
+	if err := res.ResponseMessage(c.ShouldBindJSON(&request)); len(err) > 0 {
+		allErrors = append(allErrors, err...)
+	}
+
+	// Validate the request
+	if err := request.Validate(); len(err) > 0 {
+		allErrors = append(allErrors, err...)
+	}
+
+	if len(allErrors) > 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": allErrors,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"schedule": request})
 }
--- request/schedule/scheduleRequest.go
+++ request/schedule/scheduleRequest.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"fmt"
+
 	req "github.com/edulink-api/request"
 	"github.com/edulink-api/res"
 )
@@ -10,14 +12,41 @@ import (
 * see the documentation about binding and validation here:
 * https://blog.logrocket.com/gin-binding-in-go-a-tutorial-with-examples/#validating-date-time
  */
-type InsertScheduleRequest struct {
+type ScheduleRequest struct {
+	TeacherID    int64   `json:"teacher_id" binding:"required" validate:"required"`
+	TeachingHour int32   `json:"teaching_hour" binding:"required" validate:"required"`
+	ClassNameID  []int64 `json:"class_name_id" binding:"required" validate:"required,min=1,dive"`
+	SubjectID    []int64 `json:"subject_id" binding:"required" validate:"required,min=1,dive"`
 }
 
 // Validate method
-func (r *InsertScheduleRequest) Validate() []map[string]string {
+func (r *ScheduleRequest) Validate() []map[string]string {
 
 	// Validate the struct
 	err := res.ResponseMessage(req.Validate.Struct(r))
 
 	return err
+}
+
+type InsertScheduleRequest struct {
+	ScheduleRequest []ScheduleRequest `json:"schedule" binding:"required"`
+}
+
+// Validate method
+func (r *InsertScheduleRequest) Validate() []map[string]string {
+	// Validate the struct
+	var allErrors []map[string]string
+	for i, data := range r.ScheduleRequest {
+		if err := data.Validate(); err != nil {
+			// index with error
+			errorMap := map[string]string{
+				"row-error": fmt.Sprintf("%d", i+1),
+				"field":     err[0]["field"],
+				"message":   err[0]["message"],
+			}
+			allErrors = append(allErrors, errorMap)
+		}
+	}
+
+	return allErrors
 }
